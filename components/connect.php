08f9bb20f40b4cb3162b9a2015e79eae9e38eb95
welthastore <146720515+welthastore@users.noreply.github.com>
2023-10-02 13:58:13 +0200

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=srv736.hstgr.io;dbname=u696113826_form';
+$user_name = 'u696113826_welthastore';
+$user_password = '#Kamogelo25@';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
